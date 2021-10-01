013a07b381e61cff824ec79d8b7afca64804bdb2
Luc Veronneau <vertechcon@users.noreply.github.com>
2021-10-01 13:30:52 -0400

    Commons main window (#2)
    * Change the application icon to the commons one.
    
    * Create the translator component and change text on home screen to placeholder text.
    
    * Fix dependency issue. Circumvent a bug in the new version of ngx translate by using the older version.
    
    * Make the title in the navigation bar only display the name of the page the user is on. Stop it from being a link and always saying "Client"
    
    * Remove file, edit etc. from the electron window.
    
    Co-authored-by: Ada <adayetis@gmail.com>

--- Client/ClientApp/src/app/app.module.ts
+++ Client/ClientApp/src/app/app.module.ts
@@ -1,6 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -10,6 +13,10 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { TranslatorComponent } from './translator/translator.component';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -32,16 +39,13 @@ import { TranslatorComponent } from './translator/translator.component';
       }
     }),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full', data: { title: 'Home' } },
+      { path: 'counter', component: CounterComponent, data: { title: 'Counter' } },
+      { path: 'fetch-data', component: FetchDataComponent, data: { title: 'Fetch Data' } },
     ])
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
 
-export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http);
-}
