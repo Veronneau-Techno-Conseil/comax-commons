0dd4375bdc640229218e8250210f611238911e16
Mohammad M. Moshawrab <50699848+MohMsh@users.noreply.github.com>
2021-12-01 11:51:33 -0500

    Updated to .net6.0

--- Client/ClientApp/src/app/app.component.ts
+++ Client/ClientApp/src/app/app.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { TranslateService } from '@ngx-translate/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  current_title = 'Client';
-
-  constructor(private titleService: Title, private router: Router, private activatedRoute: ActivatedRoute, private translate: TranslateService) {
-    translate.setDefaultLang('en');
-  }
-
-  ngOnInit() {
-    this.router.events.pipe(
-      filter(e => e instanceof NavigationEnd),
-      map(() => {
-        return this.activatedRoute.firstChild.snapshot.data['title'];
-      })
-    ).subscribe((title: string) => {
-      this.titleService.setTitle(title);
-      this.current_title = title
-    });
-  }
-
+  title = 'ClientApp';
 }
