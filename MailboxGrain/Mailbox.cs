01fc887270bc9a25dc476d0282bc0b35d38721db
Luc Veronneau <luc.veronneau@vertechcon.net>
2022-06-03 15:59:39 -0400

    Mailbox entity

--- MailboxGrain/Mailbox.cs
+++ MailboxGrain/Mailbox.cs
@@ -1,14 +1,27 @@
 ﻿using Comax.Commons.Orchestrator.Contracts.Mailbox;
 using Orleans;
+using Orleans.Runtime;
 using System.Threading.Tasks;
 
 namespace Comax.Commons.Orchestrator.MailboxGrain
 {
     public class Mailbox : Grain, IMailbox
     {
+        public Mailbox([PersistentState("accounts")] IPersistentState<MailboxState> mbState)
+        {
+            //mbState.State
+        }
         public Task<bool> HasMail()
         {
             return Task.FromResult(false);
+        }
+
+        public override Task OnActivateAsync()
+        {
+            return Task.CompletedTask;
+            //IStreamProvider streamProvider = base.GetStreamProvider("SimpleStreamProvider");
+            //IAsyncStream<T> stream = streamProvider.GetStream<SOIEvent>(this.GetPrimaryKeyString(), "MyStreamNamespace");
+            //StreamSubscriptionHandle<T> subscription = await stream.SubscribeAsync(IAsyncObserver<T>);
         }
     }
 }
---
+++ Node.Contracts/Mailbox/MailboxState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comax.Commons.Orchestrator.Contracts.Mailbox
+{
+    public class MailboxState
+    {
+        public Guid SOISubscriptionId { get; set; }
+    }
+}
