b0fa14d8e7c9997cdccb3456d4c0c62442140372
saebpanahifar <s.panahifar@gmail.com>
2022-10-24 13:08:10 -0600

    Integration Dashboard page with SignalR

---
+++ ClientUI.Shared/Models/ItemGroup.cs
@@ -0,0 +1,10 @@
+namespace CommunAxiom.Commons.ClientUI.Shared.Models
+{
+    public enum ItemGroup
+    {
+        Task,
+        Notification,
+        UpcomingActivity,
+        ActivityStream
+    }
+}
