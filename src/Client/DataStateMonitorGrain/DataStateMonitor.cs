6acc326c1ef5fe783eb31376cd18a9930eb3741b
saebpanahifar <s.panahifar@gmail.com>
2022-12-05 12:42:48 -0700

    Added DataStateMonitorSupervisor.cs

---
+++ src/Client/DataStateMonitorGrain/DataStateMonitor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommunAxiom.Commons.Client.Contracts.Grains.DataStateMonitor;
+using CommunAxiom.Commons.Ingestion.DataSource;
+using CommunAxiom.Commons.Orleans;
+using Orleans;
+using Orleans.GrainDirectory;
+
+namespace CommunAxiom.Commons.Client.Grains.DataStateMonitorGrain
+{
+    [GrainDirectory(GrainDirectoryName = "MyGrainDirectory")]
+    public class DataStateMonitor : Grain, IDataStateMonitor, IDisposable
+    {
+        private IDisposable _dataStateTimer = null!;
+        private readonly IDataSourceFactory _dataSourceFactory;
+        private bool _disposedValue;
+
+        public DataStateMonitor(IDataSourceFactory dataSourceFactory)
+        {
+            _dataSourceFactory = dataSourceFactory;
+        }
+
+        public override Task OnActivateAsync()
+        {
+            _dataStateTimer = RegisterTimer(
+                x => Execute(),
+                true,
+                TimeSpan.FromSeconds(0),
+                TimeSpan.FromSeconds(1)
+            ); //Repeat each second to make sure cover every cron job
+
+            return Task.FromResult(base.OnActivateAsync());
+        }
+
+        public async Task Execute()
+        {
+            var dataStateBusiness = new Business(
+                new GrainFactory(this.GrainFactory),
+                GrainReference.GrainIdentity.PrimaryKeyString, _dataSourceFactory
+            );
+
+            await dataStateBusiness.Execute();
+        }
+
+        public Task UnregisterAsync()
+        {
+            Dispose(true);
+            return Task.CompletedTask;
+        }
+
+        public Task EnsureActive()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose() => Dispose(true);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposedValue) return;
+            if (disposing)
+            {
+                _dataStateTimer.Dispose();
+            }
+
+            _disposedValue = true;
+        }
+    }
+}
---
+++ src/Client/DateStateMonitorSupervisorGrain/DateStateMonitorSupervisor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CommunAxiom.Commons.Client.Contracts.Grains.DateStateMonitorSupervisor;
+using CommunAxiom.Commons.Orleans;
+using Orleans;
+using Orleans.Concurrency;
+using Orleans.Runtime;
+
+namespace CommunAxiom.Commons.Client.Grains.DateStateMonitorSupervisorGrain
+{
+    [Reentrant]
+    public class DateStateMonitorSupervisor : Grain, IDateStateMonitorSupervisor, IDisposable
+    {
+        private IDisposable _dataStateTimer = null!;
+        private readonly IPersistentState<List<string>> _keys;
+        private Business _business;
+        private bool _disposedValue;
+
+        public DateStateMonitorSupervisor([PersistentState("DateStateMonitorKeys")] IPersistentState<List<string>> keys)
+        {
+            _keys = keys;
+        }
+
+        public override Task OnActivateAsync()
+        {
+            _business = new Business(new GrainFactory(this.GrainFactory));
+            _business.Init(_keys);
+            
+            _dataStateTimer = RegisterTimer(
+                x => _business.EnsureActiveAsync(),
+                true,
+                TimeSpan.FromSeconds(0),
+                TimeSpan.FromSeconds(1)
+            ); //Repeat each second to make sure cover every cron job
+
+            return Task.FromResult(base.OnActivateAsync());
+        }
+
+        public Task RegisterAsync(string grainKey)
+        {
+            return _business.RegisterAsync(grainKey);
+        }
+
+        public Task UnregisterAsync(string grainKey)
+        {
+            return _business.UnregisterAsync(grainKey);
+        }
+        
+        public void Dispose() => Dispose(true);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposedValue) return;
+            if (disposing)
+            {
+                _dataStateTimer.Dispose();
+            }
+
+            _disposedValue = true;
+        }
+        
+    }
+}
